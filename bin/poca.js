1d1f8e52e3d51ea0f1654863228b068cc9f5c3a7
Graham Whelan <gawhelan@gmail.com>
2017-04-30 22:20:57 +0100

    [refac] Change some code styling

--- bin/poca.js
+++ bin/poca.js
@@ -10,11 +10,11 @@ let app, configData;
 let colName, scriptName, collection;
 
 try {
-    configData = config.load();
-} catch(err) {
-    log.error(err);
-    log.error('Failed to load config file. Exiting...');
-    process.exit(1);
+  configData = config.load();
+} catch (err) {
+  log.error(err);
+  log.error('Failed to load config file. Exiting...');
+  process.exit(1);
 }
 
 app = require('commander');
@@ -23,32 +23,31 @@ app.version(require('../package.json').version);
 app.arguments('<collection> <script>');
 app.parse(process.argv);
 
-
 colName = app.args[0];
 if (!colName) {
-    log.error('No collection specified.');
-    app.help();
+  log.error('No collection specified.');
+  app.help();
 }
 
 collection = config.getCollection(configData, colName);
 if (!collection) {
-    log.error('Unknown collection: ' + colName);
-    process.exit(1);
+  log.error('Unknown collection: ' + colName);
+  process.exit(1);
 }
 
 scriptName = app.args[1];
 if (!scriptName) {
-    log.error('No script specified.');
-    app.help();
+  log.error('No script specified.');
+  app.help();
 }
 
 try {
-    script.run({
-        collection: collection,
-        script: scriptName,
-        args: app.args.slice(2),
-    });
+  script.run({
+    collection: collection,
+    script: scriptName,
+    args: app.args.slice(2),
+  });
 } catch (err) {
-    log.error(err);
-    process.exit(1);
+  log.error(err);
+  process.exit(1);
 }
